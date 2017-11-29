394d27b0fd9dc4e9710cea0c608d9d9f3d074c47
Deagler <prodesignsart@gmail.com>
2017-11-29 21:37:14 +1300

    Bit of custom branding
    Added a logo to the adaptive card in the welcome intent

--- app.js
+++ app.js
@@ -8,10 +8,10 @@ server.listen(process.env.port || process.env.PORT || 3978, function () {
    console.log('%s listening to %s', server.name, server.url); 
 });
 
-var connector = new builder.ChatConnector(/*{
+var connector = new builder.ChatConnector({
     appId: "eaaf54a6-f264-4bdb-9138-6bf6fb13e1bd",
     appPassword: "pVYV582[unlnuySPGO39_?$"
-}*/);
+};
 
 
 server.post('/api/messages', connector.listen());
